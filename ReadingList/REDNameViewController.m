156982dae77a5c118c7e444703d83529a3961a66
Rafael Goncalves <ragoncalves@resource.com.br>
2016-06-02 14:49:42 -0300

    animation

--- ReadingList/REDNameViewController.m
+++ ReadingList/REDNameViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "REDNameViewController.h"
+#import "UITextField+DoneButton.h"
 
-@interface REDNameViewController ()
+@interface REDNameViewController () {
+    CGPoint initialPoint;
+}
 
 #pragma mark - ui
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *centerVerticalContainerViewConstraint;
 
 @end
 
@@ -29,8 +33,12 @@
 -(void)viewDidLoad {
     [super viewDidLoad];
     
+    initialPoint = self.containerView.frame.origin;
+    
+    [self.nameTextField addToolbar];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillOpen:) name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillClose:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillClose:) name:UIKeyboardWillHideNotification object:nil];
 
 }
 -(void)viewWillAppear:(BOOL)animated {
@@ -39,10 +47,17 @@
 
 #pragma mark - keyboard
 -(void)keyboardWillOpen:(NSNotification *)notification {
+    self.centerVerticalContainerViewConstraint.constant = - 100;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
     
 }
 -(void)keyboardWillClose:(NSNotification *)notification {
-    
+    self.centerVerticalContainerViewConstraint.constant = 0;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 #pragma mark - dealloc
