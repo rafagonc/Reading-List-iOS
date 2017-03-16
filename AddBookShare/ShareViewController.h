a6b7eb0ffee223338aa4130eea6b457c1f557ddf
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-03-16 17:48:06 -0300

    extension

---
+++ AddBookShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  AddBookShare
+//
+//  Created by Rafael Gonzalves on 16/03/17.
+//  Copyright © 2017 Rafael Gonzalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ Transferir/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  Transferir
+//
+//  Created by Rafael Gonzalves on 16/03/17.
+//  Copyright © 2017 Rafael Gonzalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
